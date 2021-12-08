cbc27a3cb531a649bd56da8092c171477af156c4
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-08 18:01:19 +0700

    Relocation

---
+++ index.js
@@ -0,0 +1,35 @@
+const { _, fs, fastGlob, scanForRoutes } = require('ndut-helper')
+const handleMisc = require('./lib/handle-misc')
+const handleRoutes = require('./lib/handle-routes')
+const handleSwagger = require('./lib/handle-swagger')
+
+const plugin = async (fastify, options = {}) => {
+  handleSwagger(fastify, options)
+  await handleRoutes(fastify, options)
+  handleMisc(fastify)
+}
+
+module.exports = async function (fastify) {
+  const { config } = fastify
+  const ndutConfig = _.find(config.nduts, { name: 'ndut-rest' }) || {}
+  ndutConfig.restDir = ndutConfig.restDir || './rest'
+  ndutConfig.prefix = ndutConfig.prefix || '/rest'
+  ndutConfig.prefixDoc = ndutConfig.prefixDoc || '/documentation'
+  ndutConfig.queryKey = {
+    pageSize: 'pageSize',
+    page: 'page',
+    offset: 'offset',
+    sort: 'sort',
+    query: 'q'
+  }
+  ndutConfig.resultKey = {
+    success: 'success',
+    data: 'data',
+    message: 'message',
+    total: 'total',
+    totalPage: 'totalPage'
+  }
+  ndutConfig.maxPageSize = 100
+
+  return { plugin, options: ndutConfig }
+}
