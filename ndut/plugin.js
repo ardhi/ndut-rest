fa034babf20857ab71a339fa324f13ebab44457d
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-09 14:07:21 +0700

    Refactor

--- ndut/plugin.js
+++ ndut/plugin.js
@@ -1,7 +1,15 @@
 const handleMisc = require('../lib/handle-misc')
 const handleRoutes = require('../lib/handle-routes')
+const handleSwagger = require('../lib/handle-swagger')
 
 module.exports = async function (scope, options) {
+  const { getConfig } = scope.ndut.helper
+  const config = getConfig()
+  if (config.httpServer.disabled) {
+    scope.log.warn('HTTP server is disabled, route generation canceled')
+    return
+  }
+  await handleSwagger.call(scope, options)
   await handleRoutes.call(scope, options)
   await handleMisc.call(scope)
 }
