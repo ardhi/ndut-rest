d10ebc0af311d5002c4ac3f1726f275e3cbac11f
Ardhi Lukianto <ardhi@lukianto.com>
2022-07-01 07:10:57 +0700

    Updates

--- ndut/helper/model/as-list-route.js
+++ ndut/helper/model/as-list-route.js
@@ -16,22 +16,47 @@ module.exports = async function (opts = {}) {
     const model = await getModelByAlias(realAlias)
     const filter = translateFilter.call(this, request.query)
     const params = await prepList(model, filter)
-    if (_.isFunction(query)) params.where = await query.call(this, params.where)
+    if (_.isFunction(query)) params.where = await query.call(this, params.where, request)
     else params.where = _.merge(params.where, query)
     const columns = getColumns.call(this, request.query.columns)
     const { user, site, rule } = request
+    if (request.query.export && !this.ndutReport) throw this.Boom.internal('ndutReportMissing')
     if (['json', 'jsonl'].includes(request.query.export)) {
       const trueJson = request.query.export === 'json'
       const options = { trueJson, columns }
-      const stream = await this.ndutApi.helper.exportJsonl({ model, params, filter: { user, site, rule }, options })
+      const stream = await this.ndutReport.helper.exportJsonl({ model, params, filter: { user, site, rule }, options })
       reply.type(trueJson ? 'application/json' : 'text/plain').send(stream)
+      return
+    }
+    if (request.query.export === 'csv') {
+      reply.header('Content-Type', 'text/csv')
+      if (!request.query.inline) reply.header('Content-Disposition', `attachment; filename=${realAlias}.csv;`)
+      const options = { columns }
+      const data = await this.ndutReport.helper.exportCsv({ model, params, filter: { user, site, rule }, options })
+      reply.send(data)
       return
     }
     if (request.query.export === 'xlsx') {
       reply.header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
       reply.header('Content-Disposition', `attachment; filename=${realAlias}.xlsx;`)
       const options = { columns }
-      const data = await this.ndutApi.helper.exportXlsx({ model, params, filter: { user, site, rule }, options })
+      const data = await this.ndutReport.helper.exportXlsx({ model, params, filter: { user, site, rule }, options })
+      reply.send(data)
+      return
+    }
+    if (request.query.export === 'html') {
+      reply.header('Content-Type', 'text/html')
+      if (!request.query.inline) reply.header('Content-Disposition', `attachment; filename=${realAlias}.html;`)
+      const options = { columns }
+      const data = await this.ndutReport.helper.exportHtml({ model, params, filter: { user, site, rule }, options })
+      reply.send(data)
+      return
+    }
+    if (request.query.export === 'pdf') {
+      reply.header('Content-Type', 'application/pdf')
+      if (!request.query.inline) reply.header('Content-Disposition', `attachment; filename=${realAlias}.pdf;`)
+      const options = { columns }
+      const data = await this.ndutReport.helper.exportPdf({ model, params, filter: { user, site, rule }, options })
       reply.send(data)
       return
     }
